c34972d1030e6453855df81857436ff48f7e02c0
Adeeb <adeeb.islam8@gmail.com>
2024-07-09 16:13:55 +0900

    updated to carla 0.9.15

--- run_mpc.sh
+++ run_mpc.sh
@@ -4,14 +4,13 @@ export PYTHONPATH=$PYTHONPATH:${PWD}/carla/PythonAPI/carla/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"${PWD}/src/mpc_controller/src/acados/lib"
 export ACADOS_SOURCE_DIR="${PWD}/src/mpc_controller/src/acados/"
 # echo ${PYTHONPATH}
-bash carla/CarlaUE4.sh -RenderOffScreen -prefernvidia &
+bash carla/CarlaUE4.sh &
 
 # bash carla/CarlaUE4.sh -prefernvidia -quality-level=Low &
 # bash carla/CarlaUE4.sh -ini:[/Script/Engine.RendererSettings]:r.GraphicsAdapter=2 -quality-level=Low &
-sleep 10
+sleep 6
 
 source devel/setup.bash
 
 roslaunch mpc_controller mpcc_all.launch
 
-/home/adeeb/Carla-MPC-Acados/carla/CARLA_0.9.13/PythonAPI/carla/
--- setup_env.sh
+++ setup_env.sh
@@ -17,19 +17,17 @@ sudo apt-get install -y libomp5
 echo "Downloading CARLA..."
 mkdir -p carla
 cd carla
-wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.13.tar.gz
-# wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/AdditionalMaps_0.9.10.1.tar.gz
-tar -xf CARLA_0.9.13.tar.gz
-# tar -xf AdditionalMaps_0.9.10.1.tar.gz
-rm CARLA_0.9.13.tar.gz
-# rm AdditionalMaps_0.9.10.1.tar.gz
+# wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.13.tar.gz
+# tar -xf CARLA_0.9.13.tar.gz
+# rm CARLA_0.9.13.tar.gz
+
+wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.15.tar.gz
+tar -xf CARLA_0.9.15.tar.gz
+# rm CARLA_0.9.15.tar.gz
+
 cd ..
 
-# Clone and build acados
-# echo "Cloning and building acados..."
-# cd src/mpc_controller/src
-# git clone https://github.com/acados/acados.git
-# cd acados
+
 
 
 git submodule update --recursive --init
@@ -44,13 +42,9 @@ make install -j4
 cd ..
 pip install -e interfaces/acados_template
 
-# Clone the ROS bridge for CARLA
-echo "Cloning ROS bridge for CARLA..."
-cd ${WORKING_DIR}/src
-git clone --recurse-submodules https://github.com/carla-simulator/ros-bridge.git
 
 # Install ROS dependencies
-cd ..
+cd ${WORKING_DIR}
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y
 
